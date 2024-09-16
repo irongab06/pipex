ba04db810d1161f52053cddd45e2b1d17f64cda7
Gabriel Cavalier <gacavali@c4r2p4.42nice.fr>
2024-09-16 09:03:57 +0200

    ajout final

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: gacavali <gacavali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 08:22:55 by gacavali          #+#    #+#             */
-/*   Updated: 2024/09/13 18:09:52 by gacavali         ###   ########.fr       */
+/*   Updated: 2024/09/16 08:55:17 by gacavali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,18 @@ int	main(int argc, char **argv, char **envp)
 	int		i;
 
 	i = 0;
-	if (((!BONUS && argc != 5) || (BONUS && argc < 5)))
+	if (BONUS && (argc == 5 && (ft_strncmp("here_doc", argv[1], 9) == 0)))
 	{
 		ft_printf("wrong number of arguments !!!!!\n");
 		return (1);
 	}
-	check_here_doc(&cmd, argv);
+	else if (((!BONUS && argc != 5) || (BONUS && argc < 5)))
+	{
+		ft_printf("wrong number of arguments !!!!!\n");
+		return (1);
+	}
+	if (BONUS)
+		check_here_doc(&cmd, argv);
 	ft_init_struct(&cmd, argc);
 	find_path(&cmd, envp);
 	i = ft_init_and_error(&cmd, i);
